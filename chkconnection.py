1cba5e027834461b35174c1de7ee02770521c1fe
user not found <arp_scan@outlook.com>
2025-06-16 15:09:25 -0400

    Update chkconnection.py

--- chkconnection.py
+++ chkconnection.py
@@ -6,7 +6,6 @@ from time import sleep
 from rich import print
 
 def chkinternet():
-	os.system('clear')
 	try:
 	    socket.create_connection(("www.google.com", 80), timeout=5)
 	    print("internet [bold green]connected[/bold green]")
